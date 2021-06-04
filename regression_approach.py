47e3f6149e14c81624373d0ec6d23e55ceeac77e
larisa <larisa.biriescu@gmail.com>
2021-06-04 17:06:19 +0300

    model is trained only on movies that were rated; current MAE=0.9046

--- regression_approach.py
+++ regression_approach.py
@@ -59,20 +59,11 @@ class MovieLensTrainDataset(Dataset):
 
         for (u, i) in (user_item_set):
             print(f"CNT: {cnt}/{len(user_item_set)}")
-            cnt+=1
+            cnt += 1
             users.append(u)
             items.append(i)
             labels.append(ratings[(ratings['userId'] == u) & (ratings['movieId'] == i)].values[0][2])
 
-            for _ in range(num_negatives):  # TODO: eliminate this and try with all movies
-                negative_item = np.random.choice(all_movieIds)
-
-                # check that the user has not interacted with this item
-                while (u, negative_item) in user_item_set:
-                    negative_item = np.random.choice(all_movieIds)
-                users.append(u)
-                items.append(negative_item)
-                labels.append(0)
         return torch.tensor(users), torch.tensor(items), torch.tensor(labels)
 
 
@@ -89,10 +80,12 @@ class NCF(pl.LightningModule):
     def __init__(self, num_users, num_items, ratings, all_movieIds):
         super().__init__()
 
-        emb_dim = 8  #TODO: find the best value for it
+        emb_dim = 100  #TODO: find the best value for it
+        #TODO: add a bias to the embeddings?
+        #TODO: add dropout?
         self.user_embedding = nn.Embedding(num_embeddings=num_users, embedding_dim=emb_dim)
         self.item_embedding = nn.Embedding(num_embeddings=num_items, embedding_dim=emb_dim)
-        self.fc1 = nn.Linear(in_features=16, out_features=64)
+        self.fc1 = nn.Linear(in_features=200, out_features=64)
         self.fc2 = nn.Linear(in_features=64, out_features=32)
         self.output = nn.Linear(in_features=32, out_features=1)
         self.ratings = ratings
@@ -104,7 +97,7 @@ class NCF(pl.LightningModule):
         item_embedded = self.item_embedding(item_input)
 
         #concat the 2 embedding layers
-        vector = torch.cat([user_embedded, item_embedded], dim=-1)
+        vector = torch.cat([user_embedded, item_embedded], dim=-1) # try with dot product also?
 
         vector = nn.ReLU()(self.fc1(vector))
         vector = nn.ReLU()(self.fc2(vector))
@@ -125,7 +118,7 @@ class NCF(pl.LightningModule):
 
     def train_dataloader(self):
         return DataLoader(MovieLensTrainDataset(self.ratings, self.all_movieIds),
-                          batch_size=512, num_workers=8)
+                          batch_size=32, num_workers=8)
 
 
 if __name__ == "__main__":
@@ -159,16 +152,16 @@ if __name__ == "__main__":
     model = NCF(num_users, num_items, train_ratings, all_movieIds)
 
     #train the model
-    # trainer = pl.Trainer(max_epochs=20, reload_dataloaders_every_epoch=True, progress_bar_refresh_rate=50, logger=False,
-    #                      callbacks=pl.callbacks.ModelCheckpoint(dirpath="./saved_models/"))  # took ~30 mins/epoch
-    #
-    # print("----training starting----")
-    # trainer.fit(model)
-    # print("----end training----")
+    trainer = pl.Trainer(max_epochs=10, reload_dataloaders_every_epoch=True, progress_bar_refresh_rate=50, logger=False,
+                         callbacks=pl.callbacks.ModelCheckpoint(dirpath="./saved_models/"))  # took ~30 mins/epoch
+
+    print("----training starting----")
+    trainer.fit(model)
+    print("----end training----")
 
     # reload model
-    saved_model = NCF.load_from_checkpoint("./saved_models/epoch=19-step=28399_10_neg_samples.ckpt")
-    saved_model.eval()
+    # saved_model = NCF.load_from_checkpoint("./saved_models/epoch=19-step=28399_10_neg_samples.ckpt")
+    # saved_model.eval()
 
     #test the model
     # User-item pairs for testing
@@ -177,19 +170,23 @@ if __name__ == "__main__":
     # Dict of all items that are interacted with by each user
     user_interacted_items = ratings.groupby('userId')['movieId'].apply(list).to_dict()
 
+    pred_arr = []
     for (u, i) in test_user_item_set:
-        # predicted_labels = np.squeeze(saved_model(torch.tensor([u]*100),
-        #                                     torch.tensor(test_items)).detach().numpy())
+        predicted_labels = np.squeeze(model(torch.tensor([u]),
+                                            torch.tensor([i])).detach().numpy())
+        # predicted_labels = np.squeeze(saved_model(torch.tensor([u]),
+        #                                           torch.tensor([i])).detach().numpy())
 
-        predicted_labels = np.squeeze(saved_model(torch.tensor([u]),
-                                                  torch.tensor([i])).detach().numpy())
-
-        print(f"{scaler.inverse_transform(predicted_labels.reshape(-1, 1))} "
+        print(f"{scaler.inverse_transform(predicted_labels.reshape(-1, 1))[0][0]} "
               f"real rating: "
-              f"{scaler.inverse_transform(test_ratings[(test_ratings['userId'] == u)  & (test_ratings['movieId']==i)].values[0][2].reshape(-1, 1))}")
+              f"{scaler.inverse_transform(test_ratings[(test_ratings['userId'] == u)  & (test_ratings['movieId']==i)].values[0][2].reshape(-1, 1))[0][0]}")
 
-        # print(f"predicted labels: {scaler.inverse_transform(predicted_labels[np.argsort(-predicted_labels)].reshape(-1, 1))}")
-        # print(f"Predicted ratings: {scaler.inverse_transform(predicted_labels[np.argsort(-predicted_labels)].reshape(-1, 1))[-10:-1]}")
-        # break
+        pred_arr.append([u, i, format(scaler.inverse_transform(predicted_labels.reshape(-1, 1))[0][0], '.3f'),
+                         format(scaler.inverse_transform(test_ratings[(test_ratings['userId'] == u) &
+                                                                      (test_ratings['movieId'] == i)].values[0][2].reshape(-1, 1))[0][0], '.3f')])
+
+    pred_df = pd.DataFrame(data=pred_arr, columns=["userId", "movieId", "pred_rating", "real_rating"])
+    pred_df.to_csv("predictions.csv", index=False)
+    print(f"MAE test set : {(abs(pred_df['pred_rating']-pred_df['real_rating'])).sum()/len(pred_df)}")
 
     print(f"Execution time: {(time.time()-start)/60} mins")
